KDE/libkleo

commit 69fa56358784f3793129dc8021cd7dc8f70668a9

Fix filtering for multiple uids
Before this only the primaryUID was used for name and email
matching. Now all UIDs are searched. While this decreases
perfomance it is the correct behavior.

src/models/keylistsortfilterproxymodel.cpp
@@ -182,6 +182,10 @@ bool KeyListSortFilterProxyModel::filterAcceptsRow(int source_row, const QModelI
     const QRegExp rx = filterRegExp();
     const QModelIndex nameIndex = sourceModel()->index(source_row, PrettyName, source_parent);
 
+    const KeyListModelInterface *const klm = dynamic_cast<KeyListModelInterface *>(sourceModel());
+    Q_ASSERT(klm);
+    const Key key = klm->key(nameIndex);
+
     if (col) {
         const QModelIndex colIdx = sourceModel()->index(source_row, col, source_parent);
         const QString content = colIdx.data(role).toString();
@@ -189,26 +193,21 @@ bool KeyListSortFilterProxyModel::filterAcceptsRow(int source_row, const QModelI
             return false;
         }
     } else {
-        // By default match name and email
-        const QString name = nameIndex.data(role).toString();
-
-        const QModelIndex emailIndex = sourceModel()->index(source_row, PrettyEMail, source_parent);
-        const QString email = emailIndex.data(role).toString();
-
-        if (!name.contains(rx) && !email.contains(rx)) {
-            return false;
+        // By default match name and email of any UID
+        Q_FOREACH(const auto uid, key.userIDs()) {
+            const auto name = QString::fromUtf8(uid.name());
+            const auto email = QString::fromUtf8(uid.email());
+            if (name.contains(rx) || email.contains(rx)) {
+                return true;
+            }
         }
+        return false;
     }
 
     //
     // 2. Check that key filters match (if any are defined)
     //
     if (d->keyFilter) {   // avoid artifacts when no filters are defined
-
-        const KeyListModelInterface *const klm = dynamic_cast<KeyListModelInterface *>(sourceModel());
-        assert(klm);
-        const Key key = klm->key(nameIndex);
-
         return d->keyFilter->matches(key, KeyFilter::Filtering);
     }
 

commit 95d5051c1ef42c27c665369f48048e02a4c61f91

Make keyFilter mechanism work again
Accidentally got broken with 69fa5635 as it always returned
the value based on the string filter

src/models/keylistsortfilterproxymodel.cpp
@@ -197,11 +197,10 @@ bool KeyListSortFilterProxyModel::filterAcceptsRow(int source_row, const QModelI
         Q_FOREACH(const auto uid, key.userIDs()) {
             const auto name = QString::fromUtf8(uid.name());
             const auto email = QString::fromUtf8(uid.email());
-            if (name.contains(rx) || email.contains(rx)) {
-                return true;
+            if (!name.contains(rx) && !email.contains(rx)) {
+                return false;
             }
         }
-        return false;
     }
 
     //