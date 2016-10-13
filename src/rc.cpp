8f891b067e1b1320248c0bbde5c3bae6ab5b199f
l10n daemon script <scripty@kde.org>
2016-10-13 07:36:29 +0000

    SVN_SILENT made messages (after extraction)

---
+++ src/rc.cpp
@@ -0,0 +1,33 @@
+// i18n: file: ui/directoryserviceswidget.ui:14
+// i18n: ectx: property (windowTitle), widget (QWidget, DirectoryServicesWidget)
+i18n("Directory Services Configuration");
+// i18n: file: ui/directoryserviceswidget.ui:26
+// i18n: ectx: property (text), widget (QLabel, x500LA)
+i18n("Directory services:");
+// i18n: file: ui/directoryserviceswidget.ui:35
+// i18n: ectx: property (whatsThis), widget (QTreeView, treeView)
+i18n("This is a list of all directory services that are configured for use with X.509 and OpenPGP.");
+// i18n: file: ui/directoryserviceswidget.ui:62
+// i18n: ectx: property (toolTip), widget (QToolButton, newTB)
+i18n("Click to add a service");
+// i18n: file: ui/directoryserviceswidget.ui:65
+// i18n: ectx: property (whatsThis), widget (QToolButton, newTB)
+i18n("Click this button to create a new directory service entry as a clone of the currently selected one (or with default values, if no other is selected). You can then configure details in the table on the left hand.");
+// i18n: file: ui/directoryserviceswidget.ui:68
+// i18n: ectx: property (text), widget (QToolButton, newTB)
+i18n("New");
+// i18n: file: ui/directoryserviceswidget.ui:90
+// i18n: ectx: property (toolTip), widget (QToolButton, deleteTB)
+i18n("Click to remove the currently selected service");
+// i18n: file: ui/directoryserviceswidget.ui:93
+// i18n: ectx: property (whatsThis), widget (QToolButton, deleteTB)
+i18n("Click this button to remove the currently selected directory service. The change will only take effect once you acknowledge the main configuration dialog.");
+// i18n: file: ui/directoryserviceswidget.ui:96
+// i18n: ectx: property (text), widget (QToolButton, deleteTB)
+i18n("Delete");
+// i18n: file: ui/directoryserviceswidget.ui:126
+// i18n: ectx: property (whatsThis), widget (QCheckBox, showUserAndPasswordCB)
+i18n("Use this option to switch display of username and password information on or off in the above table.");
+// i18n: file: ui/directoryserviceswidget.ui:129
+// i18n: ectx: property (text), widget (QCheckBox, showUserAndPasswordCB)
+i18n("Show user and password information");
