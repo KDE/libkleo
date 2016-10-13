15796386f562b0ea09a02a1cc0b6339c15854fe6
Burkhard Lück <lueck@hube-lueck.de>
2016-10-13 12:45:42 +0200

    fix Messages.sh, subdir backengs was deleted

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 $EXTRACTRC */*.ui >> rc.cpp || exit 11
-$XGETTEXT *.cpp kleo/*.cpp kleo/*.h ui/*.cpp ui/*.h \
-	backends/*/*.cpp backends/*/*.h -o $podir/libkleopatra.pot
+$XGETTEXT *.cpp kleo/*.cpp kleo/*.h ui/*.cpp ui/*.h -o $podir/libkleopatra.pot
