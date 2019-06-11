KDE/libkleo

commit 4847fcc27afc8101752de82b0dd1f5fee027695d

Extend keylistmodel with additional columns
This will allow kleo to have more column options
in the keytreeview.

src/models/keylistmodelinterface.h
@@ -60,21 +60,21 @@ public:
         ValidFrom,
         ValidUntil,
         TechnicalDetails,
-        /* OpenPGP only, really */
         ShortKeyID,
         KeyID,
-        Summary, // Short summary line
-#if 0
         Fingerprint,
-        LongKeyID,
-        /* X509 only, really */
         Issuer,
-        Subject,
         SerialNumber,
+        OwnerTrust,
+        Origin,
+        LastUpdate,
+#if 0
+        LongKeyID,
 #endif
 
         Validity,
         NumColumns,
+        Summary, // Short summary line
         Icon = PrettyName // which column shall the icon be displayed in?
     };
 

CMakeLists.txt
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.5)
-set(PIM_VERSION "5.11.40")
+set(PIM_VERSION "5.11.41")
 
 project(libkleo VERSION ${PIM_VERSION})
 

src/models/keylistmodel.cpp
@@ -198,9 +198,15 @@ QVariant AbstractKeyListModel::headerData(int section, Qt::Orientation o, int ro
             case Validity:         return i18n("User-IDs");
             case ValidFrom:        return i18n("Valid From");
             case ValidUntil:       return i18n("Valid Until");
-            case TechnicalDetails: return i18n("Details");
+            case TechnicalDetails: return i18n("Protocol");
             case ShortKeyID:       return i18n("Key-ID");
             case KeyID:            return i18n("Key-ID");
+            case Fingerprint:      return i18n("Fingerprint");
+            case Issuer:           return i18n("Issuer");
+            case SerialNumber:     return i18n("Serial Number");
+            case Origin:           return i18n("Origin");
+            case LastUpdate:       return i18n("Last Update");
+            case OwnerTrust:       return i18n("Certification Trust");
             case NumColumns:;
             }
     return QVariant();
@@ -270,13 +276,25 @@ QVariant AbstractKeyListModel::data(const QModelIndex &index, int role) const
             return Formatting::prettyID(key.keyID());
         case Summary:
             return Formatting::summaryLine(key);
+        case Fingerprint:
+            return Formatting::prettyID(key.primaryFingerprint());
+        case Issuer:
+            return QString::fromUtf8(key.issuerName());
+        case Origin:
+            return Formatting::origin(key.origin());
+        case LastUpdate:
+            return Formatting::dateString(key.lastUpdate());
+        case SerialNumber:
+            return QString::fromUtf8(key.issuerSerial());
+        case OwnerTrust:
+            return Formatting::ownerTrustShort(key.ownerTrust());
         case NumColumns:
             break;
         }
     } else if (role == Qt::ToolTipRole) {
         return Formatting::toolTip(key, toolTipOptions());
     } else if (role == Qt::FontRole) {
-        return KeyFilterManager::instance()->font(key, (column == ShortKeyID || column == KeyID) ? QFont(QStringLiteral("monospace")) : QFont());
+        return KeyFilterManager::instance()->font(key, (column == ShortKeyID || column == KeyID || column == Fingerprint) ? QFont(QStringLiteral("monospace")) : QFont());
     } else if (role == Qt::DecorationRole) {
         return column == Icon ? returnIfValid(KeyFilterManager::instance()->icon(key)) : QVariant();
     } else if (role == Qt::BackgroundRole) {

src/utils/formatting.cpp
@@ -503,6 +503,11 @@ QDate expiration_date(const T &tee)
 }
 }
 
+QString Formatting::dateString(time_t t)
+{
+    return date2string(time_t2date(t));
+}
+
 QString Formatting::expirationDateString(const Key &key)
 {
     return expiration_date_string(key.subkey(0));
@@ -1029,4 +1034,26 @@ QString Formatting::prettyID(const char *id)
         return QString();
     }
     return QString::fromLatin1(id).toUpper().replace(QRegularExpression(QStringLiteral("(....)")), QStringLiteral("\\1 ")).trimmed();
+}
+
+QString Formatting::origin(int o)
+{
+    switch (o) {
+        case Key::OriginKS:
+            return i18n("Keyserver");
+        case Key::OriginDane:
+            return QStringLiteral("DANE");
+        case Key::OriginWKD:
+            return QStringLiteral("WKD");
+        case Key::OriginURL:
+            return QStringLiteral("URL");
+        case Key::OriginFile:
+            return i18n("File import");
+        case Key::OriginSelf:
+            return i18n("Generated");
+        case Key::OriginOther:
+        case Key::OriginUnknown:
+        default:
+          return i18n("Unknown");
+    }
 }

src/utils/formatting.h
@@ -107,6 +107,9 @@ KLEO_EXPORT QDate creationDate(const GpgME::Key &key);
 KLEO_EXPORT QDate creationDate(const GpgME::Subkey &subkey);
 KLEO_EXPORT QDate creationDate(const GpgME::UserID::Signature &sig);
 
+/* Convert a GPGME style time to a localized string */
+KLEO_EXPORT QString dateString(time_t t);
+
 KLEO_EXPORT QString displayName(GpgME::Protocol prot);
 KLEO_EXPORT QString type(const GpgME::Key &key);
 KLEO_EXPORT QString type(const GpgME::Subkey &subkey);
@@ -155,6 +158,9 @@ KLEO_EXPORT QString complianceStringForKey(const GpgME::Key &key);
  * given key, including any conformance statements relevant to the
  * current conformance mode.  */
 KLEO_EXPORT QString complianceStringShort(const GpgME::Key &key);
+
+/* The origin of the key mapped to a localized string */
+KLEO_EXPORT QString origin(int o);
 }
 }
 

commit 2b0c2bd30d39d0ec568da788a8e77bc34e227c0c

Fix key mapping based on summary column
If we move the summary column below num columns data queries
for this column fail. This breaks for example the
certificatelineedit in Kleopatra.

The summary column is still hidden because we do not set
header data.

GnuPG-Bug-ID: T4569

This fixes: 4847fcc27afc8101752de82b0dd1f5fee027695d

Merge is not neccessary as 4847fcc27 was not included
in any branch so far.

src/models/keylistmodelinterface.h
@@ -73,8 +73,8 @@ public:
 #endif
 
         Validity,
-        NumColumns,
         Summary, // Short summary line
+        NumColumns,
         Icon = PrettyName // which column shall the icon be displayed in?
     };
 