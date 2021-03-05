f3d78895d1f41208b0f3333370cf6a0c38d22303
Ingo Klöcker <dev@ingo-kloecker.de>
2021-03-05 16:46:01 +0100

    Add compatibility helper for new CryptoConfig::entry() overload
    GnuPG-bug-id: 5217

---
+++ src/utils/compat.cpp
@@ -0,0 +1,37 @@
+/*
+    utils/compat.cpp
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "compat.h"
+
+#include <QGpgME/CryptoConfig>
+
+#include <gpgme++/gpgmepp_version.h>
+#if GPGMEPP_VERSION >= 0x10F02 // 1.15.2
+# define CRYPTOCONFIG_HAS_GROUPLESS_ENTRY_OVERLOAD
+#endif
+
+using namespace QGpgME;
+
+QGpgME::CryptoConfigEntry *Kleo::getCryptoConfigEntry(const CryptoConfig *config, const char *componentName, const char *entryName)
+{
+#ifdef CRYPTOCONFIG_HAS_GROUPLESS_ENTRY_OVERLOAD
+    return config->entry(QString::fromLatin1(componentName), QString::fromLatin1(entryName));
+#else
+    const CryptoConfigComponent *const comp = config->component(QString::fromLatin1(componentName));
+    const QStringList groupNames = comp->groupList();
+    for (const auto &groupName : groupNames) {
+        const CryptoConfigGroup *const group = comp ? comp->group(groupName) : nullptr;
+        if (CryptoConfigEntry *const entry = group->entry(QString::fromLatin1(entryName))) {
+            return entry;
+        }
+    }
+    return nullptr;
+#endif
+}
---
+++ src/utils/compat.h
@@ -0,0 +1,29 @@
+/*
+    utils/compat.h
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef LIBKLEO_COMPAT_H
+#define LIBKLEO_COMPAT_H
+
+#include "kleo_export.h"
+
+namespace QGpgME
+{
+class CryptoConfig;
+class CryptoConfigEntry;
+}
+
+namespace Kleo
+{
+
+KLEO_EXPORT QGpgME::CryptoConfigEntry *getCryptoConfigEntry(const QGpgME::CryptoConfig *config, const char *componentName, const char *entryName);
+
+}
+
+#endif // LIBKLEO_COMPAT_H
