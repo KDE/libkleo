5b3e58658cd795dac5872253a55f2c4792fd24af
Ingo Klöcker <dev@ingo-kloecker.de>
2021-10-29 10:55:12 +0200

    Add function for retrieving a list of available smart card readers
    Extracted from ReaderStatus in kleopatra.
    
    GnuPG-bug-id: 5666

---
+++ src/utils/scdaemon.cpp
@@ -0,0 +1,55 @@
+/*
+    utils/scdaemon.cpp
+
+    This file is part of libkleopatra
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include <config-libkleo.h>
+
+#include "scdaemon.h"
+
+#include "utils/assuan.h"
+#include "utils/hex.h"
+#include "utils/stringutils.h"
+
+#if __has_include(<QGpgME/Debug>)
+# include <QGpgME/Debug>
+#endif
+
+#include <gpgme++/context.h>
+
+#include "libkleo_debug.h"
+
+using namespace Kleo;
+using namespace GpgME;
+
+std::vector<std::string> Kleo::SCDaemon::getReaders(Error &err)
+{
+    std::vector<std::string> result;
+
+    auto c = Context::createForEngine(AssuanEngine, &err);
+    if (err) {
+        qCDebug(LIBKLEO_LOG) << "Creating context for Assuan engine failed:" << err;
+        return result;
+    }
+
+    auto assuanContext = std::shared_ptr<Context>(c.release());
+    const std::string command = "SCD GETINFO reader_list";
+    const auto readersData = Assuan::sendDataCommand(assuanContext, command.c_str(), err);
+    if (err) {
+        return result;
+    }
+
+    const auto readers = hexdecode(readersData);
+    result = split(readers, '\n');
+    // remove empty entries; in particular, the last entry
+    result.erase(std::remove_if(std::begin(result), std::end(result),
+                                std::mem_fn(&std::string::empty)),
+                 std::end(result));
+
+    return result;
+}
---
+++ src/utils/scdaemon.h
@@ -0,0 +1,38 @@
+/*
+    utils/scdaemon.h
+
+    This file is part of libkleopatra
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "kleo_export.h"
+
+namespace GpgME
+{
+class Error;
+}
+
+namespace Kleo
+{
+/** This namespace collects higher-level functions for retrieving information
+ *  from the GnuPG smart card daemon. */
+namespace SCDaemon
+{
+
+/** Returns the list of available smart card readers. If an error occurred,
+ *  then @p err provides details.
+ *  The returned strings are mostly useful for configuring the reader to use
+ *  via the reader-port option of scdaemon.
+ */
+KLEO_EXPORT std::vector<std::string> getReaders(GpgME::Error &err);
+
+}
+}
