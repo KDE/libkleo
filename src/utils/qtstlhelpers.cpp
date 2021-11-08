33f060ac5e5133caa86ce67a83f550a9d4506421
Ingo Klöcker <dev@ingo-kloecker.de>
2021-11-08 14:32:59 +0100

    Add helper for converting a QStringList to a vector of std::strings
    GnuPG-bug-id: 5638

---
+++ src/utils/qtstlhelpers.cpp
@@ -0,0 +1,25 @@
+/*
+    utils/qtstlhelpers.cpp
+
+    This file is part of libkleopatra
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include <config-libkleo.h>
+
+#include "qtstlhelpers.h"
+
+std::vector<std::string> Kleo::toStdStrings(const QList<QString> &list)
+{
+    std::vector<std::string> result;
+
+    result.reserve(list.size());
+    std::transform(std::begin(list), std::end(list),
+                   std::back_inserter(result),
+                   std::mem_fn(&QString::toStdString));
+
+    return result;
+}
---
+++ src/utils/qtstlhelpers.h
@@ -0,0 +1,27 @@
+/*
+    utils/qtstlhelpers.h
+
+    This file is part of libkleopatra
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QList>
+
+#include <string>
+#include <vector>
+
+#include "kleo_export.h"
+
+class QString;
+
+namespace Kleo
+{
+
+KLEO_EXPORT std::vector<std::string> toStdStrings(const QList<QString> &list);
+
+}
