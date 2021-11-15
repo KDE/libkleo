75e0d5096e5d876ddac78cba2cb675db7649ae86
Ingo Klöcker <dev@ingo-kloecker.de>
2021-11-15 15:22:43 +0100

    Add helpers for reading/writing key groups from/to a file
    These helpers are mainly meant to be used for import and export of
    key groups. Unlike KeyGroupConfig which uses KConfig, these helpers
    use QSettings for reading/writing the groups from/to a file. QSettings
    is used for reading because KConfig always reads global settings
    additionally to the specified file. This could lead to unexpected
    results, if any of the global settings files contains config groups
    that are or look like key groups. For symmetry and to avoid format
    problems, QSettings is also used for writing key groups to a file.
    
    GnuPG-bug-id: 5638

---
+++ src/kleo/keygroupimportexport.cpp
@@ -0,0 +1,110 @@
+/*
+    kleo/keygroupimportexport.cpp
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include <config-libkleo.h>
+
+#include "keygroupimportexport.h"
+
+#include "debug.h"
+#include "keygroup.h"
+
+#include "models/keycache.h"
+#include "utils/keyhelpers.h"
+#include "utils/qtstlhelpers.h"
+
+#include <QFile>
+#include <QSettings>
+#include <QString>
+
+#include "libkleo_debug.h"
+
+using namespace Kleo;
+using namespace GpgME;
+
+// use a different, less generic prefix for the config group names than in
+// KeyGroupConfig to avoid problems with "Group-*" config groups created by
+// other applications; this means that the key groups stored in the normal group
+// configuration file cannot be read with the below functions, but that's a good
+// thing because the ini files created by KConfig are incompatible with QSettings
+static const QString groupNamePrefix = QStringLiteral("KeyGroup-");
+
+static KeyGroup readGroup(const QSettings &groupsConfig, const QString &groupId)
+{
+    const auto configGroupPath = groupNamePrefix + groupId + QLatin1Char{'/'};
+
+    const auto groupName = groupsConfig.value(configGroupPath + QLatin1String{"Name"}, QString()).toString();
+    const auto fingerprints = groupsConfig.value(configGroupPath + QLatin1String{"Keys"}, QStringList()).toStringList();
+    const std::vector<Key> groupKeys = KeyCache::instance()->findByFingerprint(toStdStrings(fingerprints));
+
+    KeyGroup g(groupId, groupName, groupKeys, KeyGroup::ApplicationConfig);
+    qCDebug(LIBKLEO_LOG) << __func__ << "Read group" << g;
+
+    return g;
+}
+
+std::vector<KeyGroup> Kleo::readKeyGroups(const QString &filename)
+{
+    std::vector<KeyGroup> groups;
+
+    if (filename.isEmpty()) {
+        return groups;
+    }
+
+    if (!QFile::exists(filename)) {
+        qCWarning(LIBKLEO_LOG) << __func__ << "File" << filename << "does not exist";
+        return groups;
+    }
+
+    const QSettings groupsConfig{filename, QSettings::IniFormat};
+    const QStringList configGroups = groupsConfig.childGroups();
+    for (const QString &configGroupName : configGroups) {
+        if (configGroupName.startsWith(groupNamePrefix)) {
+            qCDebug(LIBKLEO_LOG) << __func__ << "Reading config group" << configGroupName;
+            const QString keyGroupId = configGroupName.mid(groupNamePrefix.size());
+            if (keyGroupId.isEmpty()) {
+                qCWarning(LIBKLEO_LOG) << __func__ << "Config group" << configGroupName << "has empty group id";
+                continue;
+            }
+            groups.push_back(readGroup(groupsConfig, keyGroupId));
+        }
+    }
+
+    return groups;
+}
+
+static void writeGroup(QSettings &groupsConfig, const KeyGroup &group)
+{
+    if (group.isNull()) {
+        qCDebug(LIBKLEO_LOG) << __func__ << "Error: group is null";
+        return;
+    }
+
+    const auto configGroupName = groupNamePrefix + group.id();
+    qCDebug(LIBKLEO_LOG) << __func__ << "Writing config group" << configGroupName;
+    const auto configGroupPath = configGroupName + QLatin1Char{'/'};
+    groupsConfig.setValue(configGroupPath + QLatin1String{"Name"}, group.name());
+    groupsConfig.setValue(configGroupPath + QLatin1String{"Keys"}, Kleo::getFingerprints(group.keys()));
+}
+
+Kleo::WriteKeyGroups Kleo::writeKeyGroups(const QString &filename, const std::vector<KeyGroup> &groups)
+{
+    if (filename.isEmpty()) {
+        return WriteKeyGroups::InvalidFilename;
+    }
+
+    QSettings groupsConfig{filename, QSettings::IniFormat};
+    for (const auto &group : groups) {
+        writeGroup(groupsConfig, group);
+    }
+    // ensure that the data is written to disk before calling status()
+    groupsConfig.sync();
+    qCDebug(LIBKLEO_LOG) << __func__ << "groupsConfig.status():" << groupsConfig.status();
+    return groupsConfig.status() == QSettings::NoError ? WriteKeyGroups::Success : WriteKeyGroups::Error;
+}
---
+++ src/kleo/keygroupimportexport.h
@@ -0,0 +1,34 @@
+/*
+    kleo/keygroupimportexport.h
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2021 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "kleo_export.h"
+
+#include <vector>
+
+class QString;
+
+namespace Kleo
+{
+class KeyGroup;
+
+KLEO_EXPORT std::vector<KeyGroup> readKeyGroups(const QString &filename);
+
+enum class WriteKeyGroups
+{
+    Success,
+    InvalidFilename,
+    Error
+};
+
+KLEO_EXPORT WriteKeyGroups writeKeyGroups(const QString &filename, const std::vector<KeyGroup> &groups);
+
+}
