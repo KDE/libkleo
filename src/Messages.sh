11cb53668995af30ebdf514ac3272f23756280e2
Laurent Montel <montel@kde.org>
2021-12-08 19:32:15 +0100

    GIT_SILENT: fix some reuse lint warning

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: none
 $EXTRACTRC */*.ui >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libkleopatra.pot
 rm -f rc.cpp
