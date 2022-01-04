d192f52fa9d09d75b67d923861d8be5e67ab4260
Andre Heinecke <aheinecke@gnupg.org>
2022-01-04 13:23:06 +0100

    Add DocAction helper class
    This is an Action that checks for a specific document in
    a path and calls QDesktopServicesOpenURL on trigger if
    the file exists.

---
+++ src/kleo/docaction.cpp
@@ -0,0 +1,65 @@
+/*
+    kleo/docaction.cpp
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2022 g10 Code GmbH
+    SPDX-FileContributor: Andre Heinecke <aheinecke@g10code.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "docaction.h"
+
+#include "libkleo_debug.h"
+
+#include <QDesktopServices>
+#include <QDir>
+#include <QUrl>
+#include <QCoreApplication>
+#include <QFileInfo>
+#include <QString>
+
+#include "libkleo_debug.h"
+
+using namespace Kleo;
+
+class Kleo::DocAction::Private
+{
+public:
+    explicit Private(const QString &filename, const QString &pathHint);
+    ~Private() = default;
+
+    QString path;
+    bool isEnabled = false;
+};
+
+DocAction::Private::Private(const QString &filename, const QString &pathHint)
+{
+    QString tmp = pathHint;
+    if (!tmp.startsWith(QLatin1Char('/'))) {
+        tmp.prepend(QLatin1Char('/'));
+    }
+    QDir datadir(QCoreApplication::applicationDirPath() +
+           (pathHint.isNull() ? QStringLiteral("/../share/kleopatra") : tmp));
+
+    path = datadir.filePath(filename);
+    QFileInfo fi(path);
+    isEnabled = fi.exists();
+}
+
+DocAction::DocAction(const QIcon &icon, const QString &text, const QString &filename,
+                     const QString &pathHint,
+                     QObject *parent) :
+    QAction(icon, text, parent),
+    d(new Private(filename, pathHint))
+{
+    setVisible(d->isEnabled);
+    setEnabled(d->isEnabled);
+    connect(this, &QAction::triggered, this, [this] () {
+        if (d->isEnabled) {
+            qCDebug(LIBKLEO_LOG) << "Opening file:" << d->path;
+            QDesktopServices::openUrl(QUrl::fromLocalFile(d->path));
+        }
+    });
+}
+
+DocAction::~DocAction() = default;
---
+++ src/kleo/docaction.h
@@ -0,0 +1,59 @@
+/*
+    kleo/docaction.h
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    SPDX-FileCopyrightText: 2022 g10 Code GmbH
+    SPDX-FileContributor: Andre Heinecke <aheinecke@g10code.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "kleo_export.h"
+
+#include <memory>
+
+#include <QAction>
+
+class QIcon;
+class QString;
+
+namespace Kleo
+{
+/**
+    An action for custom documentation which is opened
+    by file. This can be used for PDF documents like the
+    GnuPG manual.
+
+    The action is disabled and invisible if the corresponding
+    file cannout be found at creation. Otherwise triggered
+    calls QDesktopServicesOpenURL on the file.
+*/
+class KLEO_EXPORT DocAction: public QAction
+{
+    Q_OBJECT
+
+public:
+    /* Create a DocAction with icon, text and file name of the document
+     *
+     * @a filename The name of the documentation file.
+     * @a pathHint A path relative to QCoreApplication::applicationDirPath() to look for the file.
+     *
+     * */
+    explicit DocAction(const QIcon &icon,
+                       const QString &text,
+                       const QString &filename,
+                       const QString &pathHint = QString(),
+                       QObject *parent = nullptr);
+
+    ~DocAction() override;
+
+    DocAction(const QString &, QObject *parent) = delete;
+    DocAction(QObject *parent) = delete;
+private:
+    class Private;
+    std::unique_ptr<Private> d;
+};
+
+} // namespace Kleo
