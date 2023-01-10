2f2109dbb9a941e0849297ac9d89887323dae72e
Ingo Klöcker <dev@ingo-kloecker.de>
2023-01-10 12:00:44 +0100

    Add C++20 compatibility helper to check if string starts with a prefix
    GnuPG-bug-id: 6325

--- src/utils/stringutils.h
+++ src/utils/stringutils.h
@@ -13,6 +13,7 @@
 #include "kleo_export.h"
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace Kleo
@@ -22,4 +23,16 @@ namespace Kleo
  *  and returns the list of those strings. */
 KLEO_EXPORT std::vector<std::string> split(const std::string &s, char c);
 
+/**
+ * Returns true if the string @p sv begins with the string @p prefix, false
+ * otherwise.
+ */
+inline bool startsWith(std::string_view sv, std::string_view prefix)
+{
+#ifdef __cpp_lib_starts_ends_with
+    return sv.starts_with(prefix);
+#else
+    return sv.substr(0, prefix.size()) == prefix;
+#endif
+}
 }
