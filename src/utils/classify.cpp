89eb200842fc76a7363eaf1aa1bd61ee118cc42e
Volker Krause <vkrause@kde.org>
2023-11-24 16:42:44 +0100

    Port isChecksumFile away from QRegExp
    (cherry picked from commit 4bf5277f4c8c9beb1f58bc534305bc1054f2e67e)

--- src/utils/classify.cpp
+++ src/utils/classify.cpp
@@ -25,7 +25,6 @@
 #include <QFileInfo>
 #include <QMap>
 #include <QMimeDatabase>
-#include <QRegExp>
 #include <QRegularExpression>
 #include <QString>
 
@@ -344,7 +343,7 @@ bool Kleo::isFingerprint(const QString &fpr)
 bool Kleo::isChecksumFile(const QString &file)
 {
     static bool initialized;
-    static QList<QRegExp> patterns;
+    static QList<QRegularExpression> patterns;
     const QFileInfo fi(file);
     if (!fi.exists()) {
         return false;
@@ -356,9 +355,9 @@ bool Kleo::isChecksumFile(const QString &file)
                 const auto patternsList = cd->patterns();
                 for (const QString &pattern : patternsList) {
 #ifdef Q_OS_WIN
-                    patterns << QRegExp(pattern, Qt::CaseInsensitive);
+                    patterns << QRegularExpression(QRegularExpression::anchoredPattern(pattern), QRegularExpression::CaseInsensitiveOption);
 #else
-                    patterns << QRegExp(pattern, Qt::CaseSensitive);
+                    patterns << QRegularExpression(QRegularExpression::anchoredPattern(pattern));
 #endif
                 }
             }
@@ -367,8 +366,8 @@ bool Kleo::isChecksumFile(const QString &file)
     }
 
     const QString fileName = fi.fileName();
-    for (const QRegExp &pattern : std::as_const(patterns)) {
-        if (pattern.exactMatch(fileName)) {
+    for (const QRegularExpression &pattern : std::as_const(patterns)) {
+        if (pattern.match(fileName).hasMatch()) {
             return true;
         }
     }
