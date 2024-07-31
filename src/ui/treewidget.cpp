d9d00eeb72b18e0dbf761d53c21a193b4e58a091
Ingo Klöcker <dev@ingo-kloecker.de>
2024-07-31 16:32:50 +0200

    Don't sort tree views/widgets unless sorting is enabled
    If sorting isn't enabled for a tree view/widget then it must not be
    sorted when restoring the column layout for the first time.
    
    This unwanted initial sorting messed up the order of the card slots in
    the new card keys view when a card of a some type was displayed for the
    first time.
    
    GnuPG-bug-id: 7018
    (cherry picked from commit 9b4d36c0666cb56bb91f520601c98cf8255a7406)

--- src/ui/treewidget.cpp
+++ src/ui/treewidget.cpp
@@ -140,7 +140,7 @@ bool TreeWidget::restoreColumnLayout(const QString &stateGroupName)
     }
 
     int sortOrder = config.readEntry("SortAscending", (int)Qt::AscendingOrder);
-    int sortColumn = config.readEntry("SortColumn", 0);
+    int sortColumn = config.readEntry("SortColumn", isSortingEnabled() ? 0 : -1);
     if (sortColumn >= 0) {
         sortByColumn(sortColumn, (Qt::SortOrder)sortOrder);
     }
