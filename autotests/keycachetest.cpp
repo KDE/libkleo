6450f801ff65fe24fe3a87520c4091e7db9904c9
Sune Vuorela <sune@vuorela.dk>
2025-08-05 14:41:37 +0200

    Don't run v5 tests if not supported by engine

--- autotests/keycachetest.cpp
+++ autotests/keycachetest.cpp
@@ -111,6 +111,8 @@ private Q_SLOTS:
             QVERIFY(!key.isNull());
             QVERIFY(std::string_view{key.primaryFingerprint()} == key_v5_curve_448_fpr);
             keyCurve448 = key;
+        } else {
+            QSKIP("Unsupported engine");
         }
     }
 
